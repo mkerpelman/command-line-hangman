f0d46535df041fd08a56b79befab8028722b5e50
Mike Work <michael.kerpelman@gmail.com>
2021-05-11 15:34:14 -0400

    update to allow saving/loading of games in progress

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require './lib/display.rb'
+require 'json'
 
 class Game
 
@@ -7,23 +8,43 @@ class Game
     ALPHABET = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
     GUESSES_ALLOWED = 8
 
-    def initialize
-        self.generate_secret_word
-        @blanks = self.retract_all_letters
-        @display = Display.new(@blanks)
-        @remaining = GUESSES_ALLOWED
+    def initialize(type)
+        if type == "1"
+            self.generate_secret_word
+            @blanks = self.retract_all_letters
+            @display = Display.new(@blanks)
+            @remaining = GUESSES_ALLOWED
+            self.play
+        else
+            puts "        Enter full filename of saved game to load (including file extension, i.e. .json): "
+            filename = gets.chomp
+            source = self.import_progress(filename)
+            saved_data = self.unpack_json(source)
+            self.load_secret_word(saved_data["secret_word"])
+            @blanks = saved_data["blanks"]
+            @display = Display.new(@blanks)
+            @wrong = saved_data["wrong"]
+            @display.wrong_letters = @wrong
+            @remaining = saved_data["remaining"]
+            self.play
+        end
     end
 
     def play
         while @remaining > 0
             if @blanks.include?("_")
                 @display.show(@remaining)
-                letter = self.get_user_guess
-                if self.scan_secret_for_letter(letter)
-                    self.right_letter(letter)
+                if self.save_game?
+                    self.export_progress
+                    break
                 else
-                    self.wrong_letter(letter)
-                    @remaining -= 1
+                    letter = self.get_user_guess
+                    if self.scan_secret_for_letter(letter)
+                        self.right_letter(letter)
+                    else
+                        self.wrong_letter(letter)
+                        @remaining -= 1
+                    end
                 end
             else
                 @display.show(@remaining)
@@ -34,11 +55,13 @@ class Game
             puts "        You won. Great job!"
             puts ""
             puts "        *********************************************"
-        else
+        elsif !self.win? && @remaining == 0
             @display.show(@remaining)
             puts "        You lost. Too bad! The word was #{@secret_word.join}."
             puts ""
             puts "        *********************************************"
+        else
+            puts "        Game saved. See you next time!" 
         end
     end
 
@@ -51,6 +74,10 @@ class Game
         file.close
     end
 
+    def load_secret_word(array)
+        @secret_word = array
+    end
+
     def retract_all_letters
         blanks_array = Array.new(0)
         @secret_word.length.times { blanks_array << "_" }
@@ -58,10 +85,10 @@ class Game
     end
 
     def get_user_guess
-        print "Please enter a letter: "
+        print "        Please enter a letter: "
         letter = gets.chomp.upcase
         until ALPHABET.include?(letter) && !@display.wrong_letters.include?(letter)
-            print "Not a valid letter. Please try again: "
+            print "        Not a valid letter. Please try again: "
             letter = gets.chomp.upcase
         end
         letter
@@ -85,5 +112,50 @@ class Game
         !(@remaining == 0) && !@blanks.include?("_")
     end
 
+    def save_game?
+        print "        Would you like to save your game and quit? (Y/N)"
+        answer = gets.chomp.upcase
+        until ["Y", "N"].include?(answer)
+            print "        Not a valid answer. Please select 'Y' or 'N': "
+            answer = gets.chomp.upcase
+        end
+        if answer == 'Y'
+            true
+        else
+            false
+        end
+    end
+
+    def export_progress
+        Dir.mkdir('savegames') unless Dir.exist?('savegames')
+        time = Time.new
+        print "        Please enter a name for your save: "
+        custom_part = gets.chomp
+        file_name = "savegames/#{custom_part}.json"
+        file = File.open(file_name, "w")
+        file.puts self.make_json_object
+        file.close
+    end
+
+    def import_progress(filename)
+        filenamepath = "savegames/#{filename}"
+        file = File.open(filenamepath, 'r')
+        return file.read
+        file.close
+    end
+
+    def make_json_object
+        for_json = {
+            :secret_word => @secret_word, 
+            :blanks => @blanks, 
+            :wrong => @display.wrong_letters,
+            :remaining => @remaining
+        }
+        JSON.generate(for_json)
+    end
+
+    def unpack_json(content)
+        JSON.parse(content)
+    end
 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,9 @@
 require './lib/game.rb'
 
-Game.new.play
+print "        Enter (1) to start a new game or enter (2) to load a saved game: "
+choice = gets.chomp
+until ["1", "2"].include?(choice)
+    print "        Selection must be (1) or (2): "
+    choice = gets.chomp
+end
+Game.new(choice)
